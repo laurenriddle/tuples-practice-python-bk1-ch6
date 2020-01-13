1c69f1506b8d28b5043a04fe78f67bc263c154f8
Lauren Riddle <lari7132@gmail.com>
2020-01-13 15:08:12 -0600

    unpacked tuple

---
+++ tuples.py
@@ -0,0 +1,20 @@
+zoo = ("panda", "rat", "chipmunk",
+"horse", "lion", "tiger", "monkey", "goose", "bear")
+print(zoo.index("tiger"))
+animal_to_find = "tiger"
+if animal_to_find in zoo:
+    # Print that the animal was found
+    print(f"{animal_to_find} was found!")
+
+animals = ("panda", "rat", "chipmunk",
+"horse", "lion", "tiger", "monkey", "goose", "bear")
+(first_animal, second_animal, third_animal, fourth_animal, fifth_animal, sixth_animal, seventh_animal, eight_animal, ninth_animal) = animals
+print(first_animal) 
+print(second_animal) 
+print(third_animal) 
+print(fourth_animal) 
+print(fifth_animal) 
+print(sixth_animal)
+print(seventh_animal)
+print(eight_animal)
+print(ninth_animal)
